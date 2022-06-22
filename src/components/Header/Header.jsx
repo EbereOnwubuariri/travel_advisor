26b81670022ef632ebf7ddb41bf048bfd76266d5
Ebere Onwubuariri <ebereruth15@gmail.com>
2022-06-22 13:47:49 -0400

    travel advisor update places

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from 'core@material-ui/core';
-import { CallMissedSharp } from '@material-ui/icons';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles'
 
 const Header = () => {
+    const classes = useStyles();
     return (
         <AppBar position="static">
-            <Toolbar ClassName={Classes.toolbar}>
-                <Typography variant="h5" ClassName={Classes.title}>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
                     Travel Advisor
                 </Typography>
                 <Box display="flex">
-                    <Typography variant="h6" ClassName={Classes.title}>
+                    <Typography variant="h6" className={classes.title}>
                         Explore new places
                     </Typography>
-                    <Autocomplete>
-                        <div className={Classes.search}>
-                            <div className={Classes.searchIcon}>
+                     {/* <Autocomplete> */} 
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
                         </div>
-                    </Autocomplete>
+                        <div>
+                            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+                        </div>
+                    {/* </Autocomplete> */} 
                 </Box>
             </Toolbar>
         </AppBar>
