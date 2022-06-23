39d18f50ca604b96f77481ac744cdcb4ec8699b3
Ebere Onwubuariri <ebereruth15@gmail.com>
2022-06-23 10:00:57 -0400

    travel advisor update map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,14 +4,11 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 
-/*import mapStyles from '../../mapStyles';*/
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
-
-    const coordinates = { lat: 0, lng: 0 };
 
     return (
         <div className={classes.mapContainer}>
@@ -22,7 +19,11 @@ const Map = () => {
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={''}
-                onChange={''}
+                onChange={(e) => {
+                    console.log(e);
+
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                }}
                 onChildClick={''}
             >
 
